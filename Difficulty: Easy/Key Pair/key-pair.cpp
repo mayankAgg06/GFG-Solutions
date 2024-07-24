138ca9c99fb3e667975228304c92d1eb3414fad2
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-07-24 22:28:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Key Pair/key-pair.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function to check if array has 2 elements
+    // whose sum is equal to the given value
+    bool hasArrayTwoCandidates(vector<int>& arr, int x) {
+        
+        if(arr.size()<2) return false;
+        
+        unordered_map<int,int>map;
+        
+        for(auto i: arr)
+        {
+            if(map[x-i])return true;
+            
+            map[i]++;
+        }
+        
+        return false;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore(); // To discard any leftover newline characters
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
