501c46c745147e9fc1055f8f6e5cc436adc48728
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2025-06-06 09:47:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,95 +1,47 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
   public:
-  
-    void bfs(vector<int>& visited, vector<int>& solution, vector<int> adjlist[], int node)
+    // Function to return Breadth First Traversal of given graph.
+    void bfsTraversal(vector<vector<int>> &adj, vector<int> &visited, vector<int> &solution, int start)
     {
         queue<int> q;
-        q.push(node);
         
-        visited[node]=true;
+        q.push(start);
+        visited[start]=1;
         
         while(!q.empty())
         {
-            int front = q.front();
-            solution.push_back(front);
+            int node = q.front();
             q.pop();
             
-            for(auto i: adjlist[front])
+            solution.push_back(node);
+            
+            for(auto children:adj[node])
             {
-                if(!visited[i])
-                {
-                    visited[i]=true;
-                    q.push(i);
+                if(!visited[children]) {
+                    q.push(children);
+                    visited[children]=1;
                 }
             }
         }
-        
     }
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+    
+    vector<int> bfs(vector<vector<int>> &adj) {
         // Code here
-        // map<int,vector<int>> adjlist;
-        
-        // for(int i=0; i<V; i++)
-        // {
-        //     for(int j=0; j<adj[i].size(); j++)
-        //     {
-        //         adjlist[i].push_back(adj[i][j]);
-        //         adjlist[adj[i][j]].push_back(i);
-        //     }
-        // }
         
         
-        vector<int> visited(V,0);
+        int n= adj.size();
+        vector<int> visited(n,0);
+        
         vector<int> solution;
         
-        // for(int i=0; i<V; i++)
-        // {
-        //     if(!visited[i])
-        //     {
-        //         bfs(visited,solution,adj,i);
-        //     }
-        // }
-        
-         bfs(visited,solution,adj,0);
+        for(int i=0; i<n; i++)
+        {
+            if(!visited[i])
+            {
+                bfsTraversal(adj,visited,solution,i);
+            }
+        }
         
         return solution;
-        
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >>
-
-            E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            // 		adj[v].push_back(u);
-        }
-        // string s1;
-        // cin>>s1;
-        Solution obj;
-        vector<int> ans = obj.bfsOfGraph(V, adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
