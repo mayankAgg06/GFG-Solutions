e1f26f02b37b9ddc73ec208999530f1fb3fc7cc5
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2025-06-06 10:19:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,80 +1,35 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
   public:
-    void dfs( vector<int>& visited, vector<int>& solution, map<int,vector<int>>& adj, int node)
+  void dfsTraversal(vector<vector<int>> &adj, vector<int> &visited, vector<int> &solution, int start)
     {
-        visited[node]=1;
-        solution.push_back(node);
+        visited[start]=1;
+        solution.push_back(start);
         
-        for(auto i: adj[node])
+        for(auto children: adj[start])
         {
-            if(!visited[i])
+            if(!visited[children])
             {
-                dfs(visited,solution,adj,i);
+                dfsTraversal(adj,visited,solution,children);
             }
         }
+        
     }
-    // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        map<int,vector<int>> adjlist;
         
-        // for(int i=0; i<=V; i++)
-        // {
-        //     adjlist[adj[i][0]].push_back(adj[i][1]);
-        //     adjlist[adj[i][1]].push_back(adj[i][0]);
-        // }
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < adj[i].size(); j++) {
-                adjlist[i].push_back(adj[i][j]);
-                adjlist[adj[i][j]].push_back(i);
-            }
-        }
+        int n= adj.size();
+        vector<int> visited(n,0);
         
-        vector<int> visited(V,0);
         vector<int> solution;
         
-        for(int i=0; i<V; i++)
+        for(int i=0; i<n; i++)
         {
             if(!visited[i])
             {
-                dfs(visited,solution,adjlist,i);
+                dfsTraversal(adj,visited,solution,i);
             }
         }
         
         return solution;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        // string s1;
-        // cin>>s1;
-        Solution obj;
-        vector<int> ans = obj.dfsOfGraph(V, adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
